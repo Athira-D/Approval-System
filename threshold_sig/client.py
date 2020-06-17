cb88252ac45284397b3b9f57384506365ea123c0
pravitha <pravithasathyavan115@gmail.com>
2020-06-17 18:44:21 +0700

    Added code

--- threshold_sig/client.py
+++ threshold_sig/client.py
@@ -1,25 +1,41 @@
+from bls.scheme import *
+from bls.utils import *
 import socket
 from threading import Thread
+key=0
 
 def receive():
     while True:
         data = c.recv(102).decode("utf8")
         print(data)
+        index = data.find(' ')
+        init = data[0:index]
+        data = data[index + 1:]
+        if init == 'K':
+        	global key
+        	key=int(data)
 
+           
 
 host = 'localhost'
-port = 12348
+port = 12342
 addr = (host,port)
 c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 x=c.connect(addr)
 print("enter your name")
-name = input()
-c.send(bytes(name,"utf8"))
+name = raw_input()
+c.send(name)
 Thread(target = receive).start()
 while True:
     #print(name,end = '')
-    data = input()
-    c.send(bytes(data,'utf8'))
+    data = raw_input()
+    index = msg.find(' ')
+    init = msg[0:index]
+    msg = msg[index + 1:]
+    if init == 'S':
+
+
+    c.send(data)
 
 
 
--- threshold_sig/server.py
+++ threshold_sig/server.py
@@ -1,45 +1,91 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from bls.scheme import *
+from bls.utils import *
+
 from threading import Thread
 import socket
 addr_client = {}
-clients  = {}
+clients = {}
+k=0
 
 def incoming_connection():
-   
+
     count = 0
-    while count<2:
-        #print("GGGG")
-        conn, client_addr = s.accept()
-        print(client_addr)
-        #print(conn)
+    while count < 3:
+
+        # print("GGGG")
+
+        (conn, client_addr) = s.accept()
+        print client_addr
+
+        # print(conn)
+
         addr_client[conn] = client_addr
-        count=count+1
-        #print (conn)
-        Thread(target = client_connection, args = (conn,)).start()
+        count = count + 1
+
+
+        # print (conn)
+
+        Thread(target=client_connection, args=(conn, )).start()
+
+    
+
+def send_key(sk,clients):
+	i=0
+	print (len(clients))
+	for sock in clients:
+		print sk[i]
+		print ("hiiiiiiiiii")
+		sock.send("hello")
+		i=i+1
+
+
 
 
 def client_connection(conn):
-    name = conn.recv(100).decode("utf8")
+    name = conn.recv(100).decode('utf8')
+    global clients
     clients[conn] = name
+    global k
+    conn.send("K "+str(sk[k]))
+    k=k+1
+    print ("in client_connection"+str(len(clients)))
 
     while True:
-        msg = conn.recv(100).decode("utf8")
-        broadcast(msg,name+":")
+        msg = conn.recv(100).decode('utf8')
+        index = msg.find(' ')
+        init = msg[0:index]
+        msg = msg[index + 1:]
+        if init == 'I':
+            broadcast(msg, name + ':')
 
 
-def broadcast(msg,prefix):
+
+
+
+
+def broadcast(msg, prefix):
     for sock in clients:
-        #print(prefix)
-        sock.send(bytes(prefix+msg,"utf8"))
+
+        # print(prefix)
+
+        sock.send(prefix + msg)
 
 
 host = 'localhost'
-port = 12348
-addr = (host,port)
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
+port = 12342
+addr = (host, port)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+(t, n) = (2, 3)
+params = setup()
+print(params)
+(sk, vk) = ttp_keygen(params, t, n)
 s.bind(addr)
 
 s.listen(5)
-print ("waiting for connection")
-Thread (target = incoming_connection).start()
-#Thread (target = incoming_connection).join()
+print 'waiting for connection'
+Thread(target=incoming_connection).start()
+
+# Thread (target = incoming_connection).join()
